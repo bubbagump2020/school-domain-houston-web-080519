232a896067ea4bdc301103a26331be97d21e9686
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-15 14:15:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,11 +15,13 @@ class School
   
   def add_student(student_name, student_grade)
     @@roster[student_grade] = []
-    if @@roster[student_name] != student_name
-      @@roster[student_grade] << student_name
-    end
+    
       
     
     
+  end
+  
+  def grade(student_grade)
+    @@roster[student_grade]
   end
 end
