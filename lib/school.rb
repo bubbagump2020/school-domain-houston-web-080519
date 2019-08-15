acfabd0a65d6151b577d31da6f4e0fdcea5eccf2
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-15 14:22:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,7 +19,7 @@ class School
     if @@roster[student_grade] == student_grade
       @@roster[student_grade].push(student_name)
     else
-      @@roster[student_grade] == student_grade
+      @@roster[student_grade] = student_grade
       @@roster[student_grade].push(student_name)
     end
     
