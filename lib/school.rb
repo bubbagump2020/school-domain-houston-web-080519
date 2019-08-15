683beca2ecc508b64aeb84bf47427ab8f5163019
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-15 14:09:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  
+  attr_accessor :name
+  
+  @@roster = {}
+  
+  def initialize(school_name)
+    @name = school_name
+    
+  end
+  
+  def roster
+    @@roster
+  end
+  
+  def add_student(student_name, student_grade)
+    @@roster[student_grade] = []
+    @@roster[student_grade] << student_name
+    
+  end
+end
